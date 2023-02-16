c27b16dc7a33a48d2f47ab472debd12b33b99561
bobo1201 <wjdqhtjd12@naver.com>
2023-02-16 19:40:15 +0900

    23/02/16 19:40

---
+++ BoardProject/src/boardproject1/Board.java
@@ -0,0 +1,13 @@
+package boardproject1;
+
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class Board {
+	private int bno;
+	private String btitle;
+	private String bcontent;
+	private String bwriter;
+	private Date bdate;
+}
